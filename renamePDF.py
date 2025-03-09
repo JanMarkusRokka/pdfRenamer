8105cd27ad11a6b110ae6593bf23cc91e9870169
JanMa <jan10markus@gmail.com>
2025-03-09 20:26:13 +0200

    Fixed text rect and pixmap inaccuracies. Added zooming.

--- renamePDF.py
+++ renamePDF.py
@@ -9,40 +9,56 @@ image = None
 def stop_app(rect, dirs):
     # Close tkinter winder
     root.destroy()
+    i = 1
+    dir_count = str(len(dirs))
     for pdf in dirs:
         doc = pymupdf.open(pdf)
         page = doc[0]
-        print(rect)
+        page.set_rotation(0)
         # Find selected text in each document
         title = page.get_text("words", rect)[0][4]
         title = title.replace("\n", "")
         doc.close()
         # Rename the file, taking path into account
+        final = pdf
         if len(title) > 0:
             while os.path.exists(os.path.dirname(pdf) + '\\' + title + '.pdf'):
                 title+=" +"
             os.rename(pdf, os.path.dirname(pdf) + '\\' + title + '.pdf')
+            final = os.path.dirname(pdf) + '\\' + title + '.pdf'
+        print(final)
+        print(str(i) + "/" + dir_count)
+        i+=1
     input("Press enter to close")
 
 class App:
     def __init__(self, root, files):
         self.root = root
+        self.zoom = 1
         # Open first doc as reference
         doc = pymupdf.open(files[0])
-        page = doc[0]
+        self.page = doc[0]
+        self.page.set_rotation(0)
 
         #print(page.mediabox_size)
 
         # Pixmap -> Image -> PhotoImage -> add to canvas
-        pix = page.get_pixmap(dpi=72)
+        pix = self.page.get_pixmap(dpi=round(72 * self.zoom)) # 72
 
         mode = "RGBA" if pix.alpha else "RGB"
         image_ = Image.frombytes(mode, (pix.width, pix.height), pix.samples)
         global image
         image = ImageTk.PhotoImage(image_)
         print(image_.size[0], image_.size[1])
+
         self.canvas = tk.Canvas(root, width=image_.size[0], height=image_.size[1], bg="white")
-        self.canvas.create_image(10, 10, anchor=tk.NW, image=image)
+        self.image_id = self.canvas.create_image(0, 0, anchor=tk.NW, image=image)
+
+        self.zoom_in = tk.Button(root, command = self.zoom_in, text = "+")
+        self.zoom_out = tk.Button(root, command = self.zoom_out, text = "-")
+
+        self.zoom_in.pack()
+        self.zoom_out.pack()
         self.canvas.pack()
 
         self.doc = doc
@@ -66,10 +82,37 @@ class App:
         self.canvas.bind("<ButtonPress-1>", self.start_drag)
         self.canvas.bind("<B1-Motion>", self.drag)
         self.canvas.bind("<ButtonRelease-1>", self.stop_drag)
+        self.draw_text_rects()
+
+    def zoom_in(self):
+        self.change_zoom(1.25)
+    def zoom_out(self):
+        self.change_zoom(0.8)
+
+    def draw_text_rects(self):
+        words = self.page.get_text("words")
+        for word in words:
+            rect = [i * self.zoom for i in word[:4]]
+            self.canvas.create_rectangle(
+                rect, outline="red", width=2
+            )
+
+    def change_zoom(self, zoom_change):
+        self.zoom = zoom_change * self.zoom
+        self.canvas.delete('all')
+        self.selected.set("Currently selected: -")
+        pix = self.page.get_pixmap(dpi=round(72 * self.zoom))  # 72
+        mode = "RGBA" if pix.alpha else "RGB"
+        image_ = Image.frombytes(mode, (pix.width, pix.height), pix.samples)
+        self.canvas.config(width=image_.size[0], height=image_.size[1])
+        global image
+        image = ImageTk.PhotoImage(image_)
+        self.image_id = self.canvas.create_image(0, 0, anchor=tk.NW, image=image)
+        self.draw_text_rects()
 
     def submit_selection(self):
         self.doc.close()
-        stop_app([self.start_x, self.start_y, self.end_x, self.end_y], self.files)
+        stop_app([i / self.zoom for i in [self.start_x, self.start_y, self.end_x, self.end_y]], self.files)
 
     def start_drag(self, event):
         self.canvas.delete(self.rect_id)
@@ -91,7 +134,11 @@ class App:
         print(f"Rectangle created from ({self.start_x}, {self.start_y}) to ({event.x}, {event.y})")
         self.end_x = event.x
         self.end_y = event.y
-        text = self.doc[0].get_text("words", [self.start_x, self.start_y, self.end_x, self.end_y])
+        # Normalize coordinates and ensure same coordinate space
+        rect = [i / self.zoom for i in [min(self.start_x, self.end_x), min(self.start_y, self.end_y), max(self.end_x, self.start_x), max(self.end_y, self.start_y)]]
+
+        text = self.doc[0].get_text("words", rect)
+
         if len(text) > 0:
             self.selected.set("Currently selected: " + (text[0][4]))
         else:
@@ -101,7 +148,7 @@ class App:
 if __name__ == "__main__":
     # Ignore first arg, since that is the name of the program
     droppedFiles = sys.argv[1:]
-    print(droppedFiles)
+    print("Counted " + str(len(droppedFiles)) + " files")
     root = tk.Tk()
     app = App(root, droppedFiles)
     root.mainloop()
